5a8ced904eb10cddf5c24ae3e1780c04e499e597
laynedesignsui <112714618+laynedesignsui@users.noreply.github.com>
2024-09-15 21:10:13 -0400

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: 'What are accordion components?',
+        answer: 'Accordion components in React are UI elements that allow users to toggle between showing and hiding content.'
+    },
+    {
+        id: '2',
+        question: 'What is the useState hook in React?',
+        answer: 'The useState hook in React is used to add state to functional components.'
+    },
+    {
+        id: '3',
+        question: 'How does React handle component re-rendering?',
+        answer: 'React re-renders components when their state or props change.'
+    },
+    {
+        id: '4',
+        question: 'What is JSX in React?',
+        answer: 'JSX stands for JavaScript XML and is a syntax extension used in React.'
+    }
+];
+
+export default data
